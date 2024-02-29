6207441035d3c1e8f362a87a4e7c766332f11b64
hernii8 <hernandelafuentelopez@gmail.com>
2024-02-29 23:52:06 +0100

    Add abstract methods

--- src/text_interface.py
+++ src/text_interface.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Tuple
 from editor import Editor
 
@@ -6,11 +6,14 @@ from editor import Editor
 class TextInterface(ABC):
     __editor: Editor
 
+    @abstractmethod
     def __init__(self, editor: Editor):
         pass
 
+    @abstractmethod
     def update(self, text: str, cursor_coordinates: Tuple[int, int]):
         pass
 
+    @abstractmethod
     def get_char(self):
         pass
