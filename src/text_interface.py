3128f8775642a506b11c7918ced5dbfa268f1ccc
hernii8 <hernandelafuentelopez@gmail.com>
2024-02-29 23:58:47 +0100

    Remove init from abstract class

--- src/text_interface.py
+++ src/text_interface.py
@@ -1,15 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Tuple
-from editor import Editor
 
 
 class TextInterface(ABC):
-    __editor: Editor
-
-    @abstractmethod
-    def __init__(self, editor: Editor):
-        pass
-
     @abstractmethod
     def update(self, text: str, cursor_coordinates: Tuple[int, int]):
         pass
